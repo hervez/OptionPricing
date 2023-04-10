611bb4d9515601d485ca06895dbdf5af8f4718cb
Herve Zumbach <herve.zumbach@gmail.com>
2023-04-10 11:16:07 +0200

    WebScrapperProvisoryFix improvement

--- WebScrapperProvisoryFix.py
+++ WebScrapperProvisoryFix.py
@@ -1,46 +1,27 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from dfply import *
 import yfinance as yf
-from ordered_set import OrderedSet
-import random
-from copy import copy
-from yahoo_fin.stock_info import *
-from yahoo_fin.options import *
-import math
-from datetime import date
 from datetime import datetime
 
-#Generating S&P 500 List of Stocks
-sp_list = tickers_sp500()
+def get_option_data(symbol, option_type, expiration_date):
+    # Convert expiration date to datetime object
+    expiration_datetime = datetime.strptime(expiration_date, "%Y-%m-%d")
 
-get_expiration_dates(sp_list[0])
+    # Create a Ticker object for the underlying symbol
+    ticker = yf.Ticker(symbol)
 
-#Creating options dataset
-expiration = get_expiration_dates(sp_list[0])[1]   #September date
-options_df = pd.DataFrame()
-for stock in sp_list:
-   try:
-      price = get_live_price(stock)           
-      option = get_calls(stock, expiration)
-      option['CurrentPrice'] = price
-      option['Ticker'] = stock
-      option['DateExp'] = expiration
-      options_df = options_df.append(option)
-      print(price)
-      print(stock)
-   except:
-      print('Option contract not available on '+ str(expiration))
-      pass
+    # Get the option chain for the specified expiration date
+    option_chain = ticker.option_chain(expiration_date)
 
-options_df = options_df[['Contract Name', 'Strike','Bid', 'Ask', 'CurrentPrice', 'Volume', 'Open Interest', 'Ticker', 'DateExp']]
+    # Get the data for the specified option type (call or put)
+    if option_type == "call":
+        option_data = option_chain.calls
+    elif option_type == "put":
+        option_data = option_chain.puts
+    else:
+        raise ValueError("Invalid option type. Must be 'call' or 'put'.")
 
-options_df = options_df[options_df.Bid != '-']
-options_df = options_df[options_df.Ask != '-']
+    return option_data
 
-options_df['AvgPrice'] = (pd.to_numeric(options_df.Bid) + pd.to_numeric(options_df.Ask)) / 2
-options_df['OptionCost'] = options_df['AvgPrice'] * 100
-options_df['BE_percent'] = ((options_df.Strike + options_df.AvgPrice)/(options_df.CurrentPrice)) - 1
-     
-print(options_df)
+if __name__ == "__main__": 
+
+   option_data = get_option_data("AAPL", "call", "2023-04-21")
+   print(option_data)

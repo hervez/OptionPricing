15ff6821d707cea72e2836d423cdcf04f8edcc66
Lirimerie <130313245+Lirimerie@users.noreply.github.com>
2023-05-24 19:00:06 +0200

    Update Calibration.py

--- Calibration.py
+++ Calibration.py
@@ -9,26 +9,59 @@ from arch import arch_model
 TO USE IT, YOU NEED AN ARRAY OF LOG RETURNS OF TIME T = 1
 """
 def stochastic_vol(returns):
+    """"
+    Uses GARCH to estimate volatility for each day in our sample.
+    You can use it if you want to calibrate the Heston Model 
+    BUT you need to have time series, you cannot just pick any random sample
+    """
     model = arch_model(returns, vol='Garch', p=1, q=1)
     results = model.fit()
-    return results
+    volatility = results.conditional_volatility
+    return volatility
 
 
 class CalibrateVanilla:
+    """
+    General interface for calibrating vanilla options
+    """
     def __init__(self, sample, volatility=None):
+        """
+        Args:
+            Sample: array containing the daily log returns of an asset, ln(St+1/St)
+            volatility: array containing the volatility of the log returns of an asset.
+                        Use it only if Sample is a time series
+            retours: will be the returns of an asset St, St+1/St
+        """
         self.sample = sample
         self.volatility = volatility
         self.retours = []
 
     def bscalibrate(self):
+        """
+        Returns the mean and volatility of an asset's log return
+        """
         mean = np.mean(self.sample)
         vol = np.std(self.sample)
-        return mean, vol
+        return mean, vol #The volatility is used to price the classical BS model, the mean was just useful for the graph in plot returns
     
     def normalpdf(self, x, mean, vol):
+        """
+        Return the Normal Law's pdf, given a mean, a volatility and a value x.
+        """
         return (1 / (vol * np.sqrt(2 * np.pi))) * np.exp(-(x - mean) ** 2 / (2 * vol ** 2))
     
     def mertonpdf(self, variables, x): #"works only for t =1!!!"
+        """
+        Gives the pdf of a Merton Jump Diffusion model, given a set of variables and x
+        !!! Here, I have chosen t = 1 in order to simplify the calculation.
+        The formula should be rewritten as it is in our paper for another t
+        The varaibles are the following:
+            Alpha : average of the log jump
+            lamda : average number of jumps
+            delta : volatility of the log jumps
+            mu : stock return on average. You never use it when you price the option but it is needed for calibration
+            sigma : volatility of the Brownian motion of the stock
+        """
         alpha, lamda, delta, mu, sigma = variables
         sum = 0
         fi = 0
@@ -43,6 +76,9 @@ class CalibrateVanilla:
         return sum
     
     def objective_merton(self, variables):
+        """
+        Log-likelihood function
+        """
         sum_diff = 0
         for obs in self.sample:
             merton_val = self.mertonpdf(variables, obs)
@@ -52,6 +88,10 @@ class CalibrateVanilla:
         return -sum_diff
     
     def merton_calibrate(self):
+        """
+        Goal: we want to maximize the log-likelihood function (minimize -1*log-likelihood function)
+        Returns: alpha, lamda, delta, mu, sigma in that order
+        """
         initial_guess = [0, 1, 0.3, 0.7, 0.3]
         bounds = [(-10, 10), (0, 3), (1e-10, None), (-10, 10), (1e-10, None)]
         result = scipy.optimize.minimize(self.objective_merton, initial_guess, method='L-BFGS-B', bounds=bounds)
@@ -59,12 +99,25 @@ class CalibrateVanilla:
         return optimized_variables
     
     def simplereturns(self):
+        """
+        Goal: fill self.retours with the returns of the assets"""
         Qt = []
         for i in range(len(self.sample)):
             Qt.append(np.exp(self.sample[i]))
         self.retours = Qt
 
     def hestonpdf(self, variables, i):
+        """
+        Gives the pdf of the Heston model for the next movement of the stock, given a set of variables and x 
+        The variables are the following:
+            Mu : Stock's log returns
+            Rho : Correlation coefficient between the Brownian motion of the stock and the BM of the variance
+            Kappa : rate of reversion to the long-term price variance
+            Eta : long-term price variance
+            Theta : volatility of the volatility
+        This method isn't reliable and can sometimes compute weird estimations.
+        The Heston method is often calibrated with the price of vanilla options and used for more exotic options
+        """
         mu, rho, kappa, eta, theta = variables
         if i == 0:
             vol = self.volatility[i] ** 2
@@ -83,6 +136,9 @@ class CalibrateVanilla:
             return multiplier * exp1 * exp2 * exp3
         
     def heston_objective(self, variables):
+        """
+        Log-likelihood function
+        """
         sum_diff = 0
         for i, obs in enumerate(self.sample[:-1]):
             heston_val = self.hestonpdf(variables, i)
@@ -92,6 +148,10 @@ class CalibrateVanilla:
         return -sum_diff
 
     def heston_calibrate(self):
+        """
+        Goal: we want to maximize the log-likelihood function (minimize -1*log-likelihood function)
+        Returns: mu, rho, kappa, eta, theta in that order
+        """
         self.simplereturns()
         initial_guess = [0.7, 0, 2, 0.5, 0.5]
         bounds = [(None, None), (-1 + 1e-10, 1 - 1e-10), (1e-10, None), (1e-10, None), (1e-10, None)]
@@ -100,14 +160,17 @@ class CalibrateVanilla:
         return optimized_variables  # Drop the mu after this
     
     def plotreturns(self):
+        """
+        Function used in order to get nice graphics for the paper
+        """
         mean, vol = self.BScalibrate()
         x = np.linspace(np.min(self.sample), np.max(self.sample), 100)
         gauss = self.normalpdf(x, mean, vol)
         variables = self.merton_calibrate()
         merton = self.mertonpdf(variables, x)
         heston = self.heston_calibrate()
-        variables = [1, 3, 2, -1, 2]
-        truemerton = self.mertonpdf(variables, x)
+        variables = [1, 3, 2, -1, 2] # A revoir
+        truemerton = self.mertonpdf(variables, x) # A revoir
         plt.figure()
         plt.hist(self.sample, density=True, label='Random Sample', bins=500)
         plt.plot(x, gauss, label='Normal Curve')

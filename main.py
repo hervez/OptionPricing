070f0139e09c7bc0d7bd1baf65458d7c9f84a5f1
Lirimerie <130313245+Lirimerie@users.noreply.github.com>
2023-05-26 16:30:09 +0200

    il faut mettre la bonne date d'évaluation pour avoir une bonne comparaison

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from Analysis import OptionAnalysis
 
 if __name__ == "__main__":
-    analyser = OptionAnalysis(underlying='AAPL', expiration_date='2023-05-26', evaluation_date='2023-05-22')
+    analyser = OptionAnalysis(underlying='AAPL', expiration_date='2023-05-26', evaluation_date='2023-05-25')
     analyser.complete_analysis()
